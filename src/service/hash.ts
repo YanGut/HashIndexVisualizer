2fe5eeaeb0608c8f6f720eb2bba0a53375015751
Yan Gutberg <guttlauncher@gmail.com>
2025-03-05 19:45:38 -0300

    :recycle: Refactor hashFunction: Update parameter name and improve hash calculation method

--- src/service/hash.ts
+++ src/service/hash.ts
@@ -1,7 +1,7 @@
-export function hashFunction(word: string, bucketSize: number): number {
-  let hash = 0;
+export function hashFunction(word: string, bucketListSize: number): number {
+  let hash: number = 60065;
   for (let i = 0; i < word.length; i++) {
-    hash = (hash * 3 + word.charCodeAt(i)) % bucketSize;
+    hash = ((hash << 5) + hash) + word.charCodeAt(i);
   }
-  return hash;
+  return Math.abs(hash % bucketListSize);
 }
